bbc2142457f39096332d1e4eed7551586a269075
Tiago Tavares <Tiago Tavares@LAPTOP-FF4J0880>
2023-06-13 16:08:50 +0100

    Change AppBar

--- calc-rent-imob/src/components/AppBar/AppBar.js
+++ calc-rent-imob/src/components/AppBar/AppBar.js
@@ -4,20 +4,14 @@ import PropTypes from 'prop-types';
 
 const AppBar = () => (
    <div className='navdisplay'>
-      {/* <nav className='navbar'>
+      <nav className='navbar'>
          <ul className='navlist'>
             <li className='navitem'><a className='navlink' href="/">Home</a></li>
             <li className='navitem'><a className='navlink' href="/about">About</a></li>
             <li className='navitem'><a  className='navlink' href="/services">Services</a></li>
             <li className='navitem'><a className='navlink' href="/contact">Contact</a></li>
          </ul>
-      </nav>    */}
-      <nav class="navMenu">
-      <a href="#">Home</a>
-      <a href="/services">Services</a>
-      <a href="/about">About</a>
-      <div class="dot"></div>
-    </nav>
+      </nav>
    </div>
 );
 
