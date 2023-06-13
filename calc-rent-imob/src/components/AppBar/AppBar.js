d5261c0a7c791f22ea2c2fd28aaa04a4609bb9a2
Tiago Tavares <Tiago Tavares@LAPTOP-FF4J0880>
2023-06-13 15:18:56 +0100

    App Bar and react redux install

---
+++ calc-rent-imob/src/components/AppBar/AppBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './AppBar.css'
+import PropTypes from 'prop-types';
+
+const AppBar = () => (
+   <div className='navdisplay'>
+      {/* <nav className='navbar'>
+         <ul className='navlist'>
+            <li className='navitem'><a className='navlink' href="/">Home</a></li>
+            <li className='navitem'><a className='navlink' href="/about">About</a></li>
+            <li className='navitem'><a  className='navlink' href="/services">Services</a></li>
+            <li className='navitem'><a className='navlink' href="/contact">Contact</a></li>
+         </ul>
+      </nav>    */}
+      <nav class="navMenu">
+      <a href="#">Home</a>
+      <a href="/services">Services</a>
+      <a href="/work">Work</a>
+      <a href="/about">About</a>
+      <div class="dot"></div>
+    </nav>
+   </div>
+);
+
+AppBar.propTypes = {};
+
+AppBar.defaultProps = {};
+
+export default AppBar;
